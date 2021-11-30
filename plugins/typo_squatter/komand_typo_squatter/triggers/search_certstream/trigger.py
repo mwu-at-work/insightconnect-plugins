4d10eed558fa5798c66536b00f908a62fe8f1da5
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2021-11-30 20:26:10 +0100

    [MC-702][MC-841][MC-842][MC-843] Update trigger

--- plugins/typo_squatter/komand_typo_squatter/triggers/search_certstream/trigger.py
+++ plugins/typo_squatter/komand_typo_squatter/triggers/search_certstream/trigger.py
@@ -31,7 +31,7 @@ class SearchCertstream(insightconnect_plugin_runtime.Trigger):
                 score = utils.score_domain(domain.lower())
 
                 # If issued from a free CA = more suspicious
-                if "Let's Encrypt" == message.get("data", {}).get("leaf_cert", {}).get("issuer", {}).get("O"):
+                if "Let's Encrypt" in message.get("data", {}).get("leaf_cert", {}).get("issuer", {}).get("O"):
                     score += 10
                 if self.query:
                     if not re.search(self.query, domain):

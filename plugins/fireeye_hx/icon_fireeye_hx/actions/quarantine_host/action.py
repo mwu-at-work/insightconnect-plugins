5e9c6c133c0604d77bfbf02f1f5b930d8ea96d38
dsliwinski-r7 <dominik_sliwinski@rapid7.com>
2022-01-25 15:05:34 +0100

    [MC-650] Add Quarantine Host action

--- plugins/fireeye_hx/icon_fireeye_hx/actions/quarantine_host/action.py
+++ plugins/fireeye_hx/icon_fireeye_hx/actions/quarantine_host/action.py
@@ -14,5 +14,4 @@ class QuarantineHost(komand.Action):
         )
 
     def run(self, params={}):
-        # TODO: Implement run function
-        return {}
+        return {Output.SUCCESS: self.connection.api.quarantine_host(params.get(Input.AGENT_ID))}
